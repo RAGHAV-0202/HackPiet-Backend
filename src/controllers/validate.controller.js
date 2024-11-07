fec83d46b684f4396bcda86fc33bbd002a48a920
Raghav Kapoor <101061750+RAGHAV-0202@users.noreply.github.com>
2024-11-07 19:21:33 +0530

    Update validate.controller.js

--- src/controllers/validate.controller.js
+++ src/controllers/validate.controller.js
@@ -30,5 +30,26 @@ const validate = async(req,res)=>{
     }
 }
 
-export {validate}
+const upload = async(req,res)=>{
+    try{
+        const {name , certificateNumber} = req.body
+        // console.log({name , certificateNumber})
+
+        if(typeof name !== "string" || typeof certificateNumber !== "string" || !name.trim() || !certificateNumber.trim()){
+            res.status(400).json("enter both name and certificate number")
+        }
+
+        const user = await User.create({Name : name , certificateID : certificateNumber})
+        
+        res.status(200).json(
+                 `${name} with certificate id ${certificateNumber} added to the Data base , with id ${user._id}`                  
+             )
+
+    }catch(error){
+        console.log("error while adding certificate")
+        res.status(500).json("couldnt upload")
+    }
+}
+
+export {validate , upload}
 
